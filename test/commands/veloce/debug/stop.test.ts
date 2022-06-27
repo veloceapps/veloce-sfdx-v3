bb01636ec76c26f7cd6874c35b0e68d9e1f648ac
vkorehov <vladislavs.korehovs@veloceapps.com>
2022-06-27 12:59:02 +0300

    adding debug tests placeholders and fixing typo in push test

--- test/commands/veloce/debug/stop.test.ts
+++ test/commands/veloce/debug/stop.test.ts
@@ -1,19 +1,20 @@
-import { promisify } from 'node:util';
-import {exec as plainExec} from 'node:child_process';
+// import { promisify } from 'node:util';
+// import {exec as plainExec} from 'node:child_process';
 
 // import { writeFileSync } from 'node:fs'
 
-const exec = promisify(plainExec);
+// const exec = promisify(plainExec);
 
-describe('veloce:source:push', () => {
-  it('should push veloce sources to org', async () => {
+describe('veloce:debug:stop', () => {
+  it('should stop debug in org', async () => {
     //     var name = `HugeCorp-${Date.now()}`
     //     const dataCSV = `Id,Name,VELOCPQ__Active__c,VELOCPQ__BundleProduct__c,VELOCPQ__Comment__c,VELOCPQ__ContentId__c,VELOCPQ__EndDate__c,VELOCPQ__Pml__c,VELOCPQ__PropertyMapId__c,VELOCPQ__ReferenceId__c,VELOCPQ__StartDate__c,VELOCPQ__UiDefinitionsId__c,VELOCPQ__Version__c
     // aBQ040000008RMfGAM,${name},false,false,"Super cool company",01504000000ISZRAA4,,,,aBQ040000008RMfDAM,,01504000000ISZWAA4,
     // `
     //    writeFileSync("/tmp/VELOCPQ__ProductModel__c.csv", dataCSV)
-    const cmdResult = await exec('sfdx veloce:source:push -u sfdx-test -m pml:OCTA -p /tmp/');
-    console.log(cmdResult.stdout);
+    // FIXME: times out!
+    // const cmdResult = await exec('sfdx veloce:debug -u sfdx-test');
+    // console.log(cmdResult.stdout);
     // var check = await exec(`sfdx force:data:soql:query  -u sfdx-test -q "select fields(all) from VELOCPQ__ProductModel__c where Name = '${name}' limit 10" --json`)
     // var checkParsed = JSON.parse(check.stdout)
     // console.log(checkParsed)
