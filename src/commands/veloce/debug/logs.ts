30a8bcd6f5f2ef74041bbbb0530b647d025ed07e
Yury Kandrashou <yury.kandrashou@veloceapps.com>
2022-06-30 12:16:12 +0300

    address review comments

--- src/commands/veloce/debug/logs.ts
+++ src/commands/veloce/debug/logs.ts
@@ -1,76 +1,82 @@
-import { readFileSync } from 'fs'
-import * as os from 'os'
-import * as path from 'path'
-import { SfdxCommand } from '@salesforce/command'
-import { Messages } from '@salesforce/core'
-import { AnyJson } from '@salesforce/ts-types'
-import { default as axios } from 'axios'
-import {EOL} from 'node:os';
+import { readFileSync } from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+import { SfdxCommand } from '@salesforce/command';
+import { Messages } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+import { AxiosError, default as axios } from 'axios';
+import { EOL } from 'node:os';
 
 // Initialize Messages with the current plugin directory
-Messages.importMessagesDirectory(__dirname)
+Messages.importMessagesDirectory(__dirname);
 
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
 // or any library that is using the messages framework can also be loaded this way.
-const messages = Messages.loadMessages('veloce-sfdx-v3', 'debug-logs')
+const messages = Messages.loadMessages('veloce-sfdx-v3', 'debug-logs');
 
 export default class Org extends SfdxCommand {
-  public static description = messages.getMessage('commandDescription')
+  public static description = messages.getMessage('commandDescription');
   public static examples = messages.getMessage('examples').split(EOL);
 
-  public static args = []
+  public static args = [];
 
-  protected static flagsConfig = {}
+  protected static flagsConfig = {};
 
   // Comment this out if your command does not require an org username
-  protected static requiresUsername = true
+  protected static requiresUsername = true;
 
   // Comment this out if your command does not support a hub org username
-  protected static supportsDevhubUsername = true
+  protected static supportsDevhubUsername = true;
 
   // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-  protected static requiresProject = false
+  protected static requiresProject = false;
 
   public async run(): Promise<AnyJson> {
-    const homedir = os.homedir()
-    const debugSessionFile = path.join(homedir, '.veloce-sfdx/debug.session')
-    let debugSession: { [key: string]: any }
+    const homedir = os.homedir();
+    const debugSessionFile = path.join(homedir, '.veloce-sfdx/debug.session');
+    let debugSession: { [key: string]: any };
     try {
-      debugSession = JSON.parse(readFileSync(debugSessionFile).toString())
+      debugSession = JSON.parse(readFileSync(debugSessionFile).toString());
     } catch (e) {
-      this.ux.log('No active debug session found, please start debug session using veloce:debug')
-      return {}
+      this.ux.log('No active debug session found, please start debug session using veloce:debug');
+      return {};
     }
 
     const params = {
-      'veloceNamespace': '',
-      'instanceUrl': `${debugSession.instanceUrl as string}`,
-      'organizationId': `${debugSession.orgId as string}`,
-      'oAuthHeaderValue': 'Dummy'
-    }
-    const authorization = Buffer.from(JSON.stringify(params)).toString('base64')
+      veloceNamespace: '',
+      instanceUrl: `${debugSession.instanceUrl as string}`,
+      organizationId: `${debugSession.orgId as string}`,
+      oAuthHeaderValue: 'Dummy',
+    };
+    const authorization = Buffer.from(JSON.stringify(params)).toString('base64');
     const headers = {
       'dev-token': debugSession.token,
-      'Authorization': authorization
-    }
-    const backendUrl: string | undefined = debugSession.backendUrl
+      Authorization: authorization,
+    };
+    const backendUrl: string | undefined = debugSession.backendUrl;
 
-    this.ux.log('Following the backend logs...')
+    this.ux.log('Following the backend logs...');
 
     await new Promise(() => void this.callToGetLogs(backendUrl, headers));
 
-    return {}
+    return {};
   }
 
-  private async callToGetLogs(backendUrl: string | undefined,
-                              headers: { Authorization: string; 'dev-token': any }): Promise<void> {
+  private async callToGetLogs(
+    backendUrl: string | undefined,
+    headers: { Authorization: string; 'dev-token': any },
+  ): Promise<void> {
     try {
-      const response = await axios.get(`${backendUrl}/services/dev-override/logs`, {headers});
+      const response = await axios.get(`${backendUrl}/services/dev-override/logs`, { headers });
       if (response.data !== '') {
-        this.ux.log(response.data)
+        this.ux.log(response.data);
       }
-    } catch (e) {
-      this.ux.log(`Failed to get logs: ${e as string}`)
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        this.ux.log(`Failed to get logs: ${error.response?.data} code ${error.response?.status}`);
+      } else {
+        this.ux.log(`Failed to get logs: ${JSON.stringify(error)}`);
+      }
     } finally {
       setTimeout(() => void this.callToGetLogs(backendUrl, headers), 1000);
     }
