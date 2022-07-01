a98893cc546f038151df6ebf1b365350cbc0d172
Aleksejs Vihrovs <aleksej.vihrovs@veloceapps.com>
2022-07-01 09:57:06 +0300

    added source:push of drools

--- src/common/push.drl.ts
+++ src/common/push.drl.ts
@@ -1,10 +1,96 @@
-import { Connection } from '@salesforce/core';
+import { Connection, SfdxError } from '@salesforce/core';
+import { ExecuteService } from '@salesforce/apex-node';
 import { Member } from '../types/member.types';
+import { extractGroupsFromFolder, Group, Rule } from '../utils/drools.utils';
 
 export interface PushDRLParams {
   rootPath: string;
   conn: Connection;
   member: Member | undefined;
+}
+
+function addRule(rule: Rule): string {
+  let code = `o.Name = '${rule.name}';\n`;
+  const action = JSON.stringify(rule.action);
+  code += `o.VELOCPQ__Action__c = '${action.substring(1, action.length - 1)}';\n`;
+  code += `o.VELOCPQ__Active__c = ${rule.active};\n`;
+  const condition = JSON.stringify(rule.condition);
+  code += `o.VELOCPQ__Condition__c = '${condition.substring(1, condition.length - 1)}';\n`;
+  code += `o.VELOCPQ__Description__c = '${rule.description || rule.name}';\n`;
+  code += `o.VELOCPQ__Sequence__c = ${rule.sequence};\n`;
+  return code;
+}
+
+async function saveRule(group: Group, rule: Rule, conn: Connection): Promise<void> {
+  let code = '';
+  code += `VELOCPQ__PriceRuleGroup__c[] gs = [SELECT Id FROM VELOCPQ__PriceRuleGroup__c WHERE VELOCPQ__ReferenceId__c = '${group.referenceId}' LIMIT 1];\n`;
+  code += 'VELOCPQ__PriceRuleGroup__c g = gs[0];\n';
+  code += `VELOCPQ__PriceRule__c[] rs = [SELECT Id, VELOCPQ__PriceRuleGroupId__c FROM VELOCPQ__PriceRule__c WHERE VELOCPQ__ReferenceId__c = '${rule.referenceId}' LIMIT 1];\n`;
+  code += 'if (rs.size() > 0){\n';
+  code += '    VELOCPQ__PriceRule__c oldRule = rs[0];\n';
+  code += '    if (oldRule.VELOCPQ__PriceRuleGroupId__c.equals(g.Id)){\n';
+  code += '        VELOCPQ__PriceRule__c o = oldRule;\n';
+  code += addRule(rule);
+  code += '        update o;  \n';
+  code += '    } else {\n';
+  code += `        VELOCPQ__PriceRule__c o = new VELOCPQ__PriceRule__c(VELOCPQ__ReferenceId__c = '${rule.referenceId}');\n`;
+  code += '        o.VELOCPQ__PriceRuleGroupId__c = g.Id;\n';
+  code += addRule(rule);
+  code += '        insert o;\n';
+  code += '        delete oldRule;\n';
+  code += '    }\n';
+  code += '} else {\n';
+  code += `    VELOCPQ__PriceRule__c o = new VELOCPQ__PriceRule__c(VELOCPQ__ReferenceId__c = '${rule.referenceId}');\n`;
+  code += addRule(rule);
+  code += '    o.VELOCPQ__PriceRuleGroupId__c = g.Id;\n';
+  code += '    insert o;\n';
+  code += '}';
+  const exec = new ExecuteService(conn);
+  const execAnonOptions = Object.assign({}, { apexCode: code });
+  const result = await exec.executeAnonymous(execAnonOptions);
+  if (!result.success) {
+    throw new SfdxError(result.logs || 'Compile Error');
+  }
+}
+
+function addGroup(group: Group): string {
+  let code = `o.Name = '${group.name}';\n`;
+  code += `o.VELOCPQ__Active__c = ${group.active};\n`;
+  code += `o.VELOCPQ__Description__c = '${group.description}';\n`;
+  code += `o.VELOCPQ__Sequence__c = ${group.sequence};\n`;
+  code += `o.VELOCPQ__Type__c = '${group.type}';\n`;
+  return code;
+}
+
+async function saveGroup(group: Group, conn: Connection): Promise<void> {
+  let code = '';
+  code += `VELOCPQ__PriceRuleGroup__c[] gs = [SELECT Id, VELOCPQ__PriceListId__c FROM VELOCPQ__PriceRuleGroup__c WHERE VELOCPQ__ReferenceId__c = '${group.referenceId}' LIMIT 1];\n`;
+  code += 'if (gs.size() > 0){\n';
+  code += '    VELOCPQ__PriceRuleGroup__c oldGroup = gs[0];\n';
+  code += `    if (oldGroup.VELOCPQ__PriceListId__c.equals('${group.priceListId}')){\n`;
+  code += '        VELOCPQ__PriceRuleGroup__c o = oldGroup;\n';
+  code += addGroup(group);
+  code += '        update o;\n';
+  code += '    } else {\n';
+  code += `        VELOCPQ__PriceRuleGroup__c o = new VELOCPQ__PriceRuleGroup__c(VELOCPQ__ReferenceId__c = '${group.referenceId}');\n`;
+  code += addGroup(group);
+  code += `        o.VELOCPQ__PriceListId__c = '${group.priceListId}';\n`;
+  code += '        insert o;\n';
+  code += '        delete oldGroup;\n';
+  code += '    }\n';
+  code += '} else {\n';
+  code += `    VELOCPQ__PriceRuleGroup__c o = gs.size() > 0 ? gs[0] : new VELOCPQ__PriceRuleGroup__c(VELOCPQ__ReferenceId__c = '${group.referenceId}');\n`;
+  code += addGroup(group);
+  code += `    o.VELOCPQ__PriceListId__c = '${group.priceListId}';\n`;
+  code += '    insert o;    \n';
+  code += '}\n';
+
+  const exec = new ExecuteService(conn);
+  const execAnonOptions = Object.assign({}, { apexCode: code });
+  const result = await exec.executeAnonymous(execAnonOptions);
+  if (!result.success) {
+    throw new SfdxError(result.logs || 'Compile Error');
+  }
 }
 
 export async function pushDRL(params: PushDRLParams): Promise<string[]> {
@@ -13,9 +99,15 @@ export async function pushDRL(params: PushDRLParams): Promise<string[]> {
     return [];
   }
   const sourcePath: string = rootPath + '/drl';
-  console.log(sourcePath);
-  console.log(conn.toString());
-  console.log(member.toString());
-  await Promise.resolve(); // todo remove, added for eslint error
+  const result = extractGroupsFromFolder(sourcePath);
+  for (const group of result) {
+    if (member.all || member.names.includes(group.name)) {
+      await saveGroup(group, conn);
+      for (const rule of group.priceRules) {
+        await saveRule(group, rule, conn);
+      }
+    }
+  }
+
   return [];
 }
--- src/utils/drools.utils.ts
+++ src/utils/drools.utils.ts
@@ -1,5 +1,6 @@
 import path = require('path');
 import fs = require('fs');
+import { SfdxError } from '@salesforce/core';
 import { parseJsonSafe } from './common.utils';
 
 const ruleExtractRegex = /(rule\b)([\S\s]*?)(end\b)/g;
@@ -7,15 +8,17 @@ const rulePreconditionRegex = /(?<=when\b)([\S\s]*?)(?=then\b)/g;
 const ruleBodyRegex = /(?<=then\b)([\S\s]*?)(?=end\b)/g;
 const ruleSequenceRegex = /(?<=salience\b)([\S\s]*?)(?=when\b)/g;
 
-interface Rule {
+export interface Rule {
   name: string;
   action: string;
   active: boolean;
   condition: string;
+  description: string;
   sequence: number;
+  referenceId: string;
 }
 
-interface Group {
+export interface Group {
   active: boolean;
   name: string;
   description: string;
@@ -26,19 +29,37 @@ interface Group {
   referenceId: string;
 }
 
-export function extractRulesFromGroup(groupFilePath: string): Rule[] {
+export function extractRulesFromGroup(groupFilePath: string, rulesMeta: Rule[]): Rule[] {
   const result: Rule[] = [];
   const rulesContent = fs.readFileSync(groupFilePath, { encoding: 'utf8' }).toString();
   const rulesRegexResults = [...(rulesContent.match(ruleExtractRegex) ?? [])];
+
+  const nameToRuleMap = rulesMeta.reduce<Record<string, Rule>>((trunk, rule) => {
+    return {
+      ...trunk,
+      [rule.name]: rule,
+    };
+  }, {});
+
   for (const rulesRegexResult of rulesRegexResults) {
     const preconditionResult = [...(rulesRegexResult.match(rulePreconditionRegex) ?? [])];
     const ruleBodyResult = [...(rulesRegexResult.match(ruleBodyRegex) ?? [])];
 
+    const name = getRuleName(rulesRegexResult);
+    const ruleMeta = nameToRuleMap[name];
+    if (!ruleMeta) {
+      throw new SfdxError(`Rule in MetaFile with 'name' ${name} is missing.`);
+    }
+    if (!ruleMeta.referenceId) {
+      throw new SfdxError(`Property 'referenceId' for rule ${ruleMeta.name} is missing.`);
+    }
     const ruleRecord = {
-      name: getRuleName(rulesRegexResult),
+      name,
       action: ruleBodyResult[0],
-      active: true,
+      active: ruleMeta.active ?? true,
       condition: preconditionResult[0],
+      description: ruleMeta.description || name,
+      referenceId: ruleMeta.referenceId,
       sequence: getSequenceNumber(rulesRegexResult),
     };
     result.push(ruleRecord);
@@ -57,18 +78,44 @@ export function getSequenceNumber(rulesRegexResultElement: string): number {
   return parseInt(sequence.toString().trim(), 10);
 }
 
-function extractGroupFromFile(groupFile: string, rulesDirectory: string): Group | undefined {
+export function validateGroupMeta(groupRecord: Group, groupFile: string): void {
+  if (!groupRecord.name) {
+    throw new SfdxError(`Property 'name' is missing from MetaFile of group ${groupFile}`);
+  }
+  if (!groupRecord.priceListId) {
+    throw new SfdxError(`Property 'priceListId' is missing from MetaFile of group ${groupFile}`);
+  }
+  if (!groupRecord.sequence) {
+    throw new SfdxError(`Property 'sequence' is missing from MetaFile of group ${groupFile}`);
+  }
+  if (!groupRecord.type) {
+    throw new SfdxError(`Property 'type' is missing from MetaFile of group ${groupFile}`);
+  }
+  if (!groupRecord.referenceId) {
+    throw new SfdxError(`Property 'referenceId' is missing from MetaFile of group ${groupFile}`);
+  }
+  if (!groupRecord.priceRules) {
+    throw new SfdxError(`List 'priceRules' is missing from MetaFile of group ${groupFile}`);
+  }
+}
+
+function extractGroupFromFile(groupFile: string, rulesDirectory: string): Group {
   const groupMetaFile = groupFile.replace('.drl', '.json');
   const groupMetaFilePath = rulesDirectory + '/' + groupMetaFile;
   const metadataStr = fs.readFileSync(groupMetaFilePath).toString();
-  const groupRecord = parseJsonSafe(metadataStr);
-  let group: Group;
-  if (groupRecord) {
+  const groupMetaData: Group = parseJsonSafe(metadataStr);
+  if (groupMetaData) {
+    validateGroupMeta(groupMetaData, groupFile);
     const groupFilePath = rulesDirectory + '/' + groupFile;
-    group = { ...groupRecord, priceRules: extractRulesFromGroup(groupFilePath) };
-    return group;
+    return {
+      ...groupMetaData,
+      active: groupMetaData.active ?? true,
+      description: groupMetaData.description || groupMetaData.name,
+      priceRules: extractRulesFromGroup(groupFilePath, groupMetaData.priceRules),
+    };
+  } else {
+    throw new SfdxError(`MetaFile is missing for group ${groupFile}`);
   }
-  return undefined;
 }
 
 export function extractGroupsFromFolder(rulesDirectory: string): Group[] {
@@ -82,10 +129,7 @@ export function extractGroupsFromFolder(rulesDirectory: string): Group[] {
   const result: Group[] = [];
 
   for (const groupFile of groupFiles) {
-    const group = extractGroupFromFile(groupFile, rulesDirectory);
-    if (group) {
-      result.push(group);
-    }
+    result.push(extractGroupFromFile(groupFile, rulesDirectory));
   }
   return result;
 }
