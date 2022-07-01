bc44d0a98318521895936de38a833207c6c991d3
Aleksejs Vihrovs <aleksej.vihrovs@veloceapps.com>
2022-07-01 17:41:52 +0300

    added pull command for drools

---
+++ src/types/priceRule.types.ts
@@ -0,0 +1,11 @@
+export interface PriceRule {
+  Id: string;
+  Name: string;
+  VELOCPQ__Action__c: string;
+  VELOCPQ__Active__c: boolean;
+  VELOCPQ__Condition__c: string;
+  VELOCPQ__Description__c: string;
+  VELOCPQ__ReferenceId__c: string;
+  VELOCPQ__PriceRuleGroupId__c: string;
+  VELOCPQ__Sequence__c: number;
+}
