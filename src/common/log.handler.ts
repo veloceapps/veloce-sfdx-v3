d5bbcabd957b07f03d2f9f1c8d4f20952c16077b
Natalija <natalija.kuzmenkova@veloceapps.com>
2022-07-04 14:15:59 +0300

    Fix logs.

--- src/common/log.handler.ts
+++ src/common/log.handler.ts
@@ -3,7 +3,7 @@ import { UX } from '@salesforce/command';
 
 export function logError(logger: UX, message: string, error: unknown): void {
   if (error instanceof AxiosError) {
-    const data = error.response?.data ? String(error.response?.data) : '';
+    const data = error.response?.data ? JSON.stringify(error.response?.data) : '';
     logger.log(`${message}: ${data} code ${error.response?.status}`);
   } else {
     logger.log(`${message}: ${JSON.stringify(error)}`);
