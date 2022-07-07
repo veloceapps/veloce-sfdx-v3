3c63dd9b26c72d7d74e3a56d584d884957607e6c
Natalija <natalija.kuzmenkova@veloceapps.com>
2022-07-07 14:36:45 +0300

    PT-2656: Add logs

--- src/common/debug.command.ts
+++ src/common/debug.command.ts
@@ -5,6 +5,7 @@ import { SfdxCommand } from '@salesforce/command';
 import { default as axios } from 'axios';
 import DebugSessionInfo from '../types/DebugSessionInfo';
 import { getDebugClientHeaders } from '../utils/auth.utils';
+import { logError } from './log.handler';
 
 export abstract class DebugSfdxCommand extends SfdxCommand {
   protected getDebugSession(): DebugSessionInfo | null {
@@ -31,7 +32,7 @@ export abstract class DebugSfdxCommand extends SfdxCommand {
         },
       );
     } catch (error) {
-      // silent call
+      logError('Failed to stop session', error);
     }
   }
 }
