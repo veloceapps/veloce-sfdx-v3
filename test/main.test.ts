326e7cc626cb8116f062f31a65ec480babad09d1
Sergejs Mankevics <sergejs.mankevics@veloceapps.com>
2022-08-26 13:56:21 +0300

    log

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,6 +3,8 @@ import { getTestEnv } from './utils';
 const env = getTestEnv();
 const restrictedEnvs = ['studio-dev'];
 
+console.log(`Running tests on ${env} org`);
+
 beforeEach(() => {
   if (restrictedEnvs.includes(env)) {
     console.error(`'${env}' org is not allowed for running tests!`);
