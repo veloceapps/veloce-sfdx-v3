3c2b44fd50184a3b11950132b166e23f52f779ae
Natalija <natalija.kuzmenkova@veloceapps.com>
2024-03-28 15:57:43 +0200

    CRQ-3871 revert idmap

--- src/common/push.docTemplate.ts
+++ src/common/push.docTemplate.ts
@@ -11,8 +11,9 @@ import {
 } from '../utils/common.utils';
 import { createOrUpdateContentDocument } from '../utils/contentDocument.utils';
 import { createTemplate, fetchTemplates, updateTemplate } from '../utils/docTemplate.utils';
-
+import { IdMap } from '../types/idmap';
 export interface PushDocTemplatesParams {
+  idmap: IdMap;
   rootPath: string;
   conn: Connection;
   member?: Member;
